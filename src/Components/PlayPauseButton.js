9d0b850520ad0892655ecf64c65702f3a39888a0
Jake Cimbalista <jcimbalista@tonicdesign.com>
2017-07-13 13:41:45 -0400

    Added time stamp using stopwatch package

--- src/Components/PlayPauseButton.js
+++ src/Components/PlayPauseButton.js
@@ -5,31 +5,38 @@ import {
   TouchableOpacity
 } from 'react-native';
 import { ReactNativeAudioStreaming } from 'react-native-audio-streaming';
+import { Stopwatch } from 'react-native-stopwatch-timer';
 var moment = require('moment');
 
 class PlayPauseButton extends React.Component {
   state = {
-    isMp3Playing: false,
-    mp3Progress: 0
+    isMp3Playing: false
   };
+
+  constructor(props) {
+    super(props);
+    this.toggleStopwatch = this.toggleStopwatch.bind(this);
+    this.resetStopwatch = this.resetStopwatch.bind(this);
+  }
+
+  toggleStopwatch() {
+    this.setState({stopwatchStart: !this.state.stopwatchStart, stopwatchReset: false});
+  }
+ 
+  resetStopwatch() {
+    this.setState({stopwatchStart: false, stopwatchReset: true});
+  }
   
+  getFormattedTime(time) {
+    this.currentTime = time;
+  };
+
   render() {
     
     let secureMp3 = this.props.mp3;
     let playableMp3 = secureMp3.replace("https", "http");
     let buttonStatus = this.state.isMp3Playing ? "Playing" : "Paused";
     let buttonStyle = this.state.isMp3Playing ? styles.button1 : styles.button2;
-
-    ReactNativeAudioStreaming.getStatus((error, mp3Status) => {
-      if (error) {
-        console.log(error)
-      } else {
-        prog = moment(mp3Status.progress * 1000).format('m:ss');
-        if (prog != this.state.mp3Progress) {
-          this.setState({mp3Progress: prog})
-        }
-      }
-    });
 
     return (
       <View style={styles.container} >
@@ -46,13 +53,21 @@ class PlayPauseButton extends React.Component {
             <Text style={styles.buttonText}>{buttonStatus}</Text>
           </View>
         </TouchableOpacity>
-          <Text>{this.state.mp3Progress}</Text>
+        <Stopwatch options={styles.stopwatch}
+          start={this.state.isMp3Playing}
+          getTime={this.getFormattedTime} />
       </View>
     )
   }
 }
 
 const styles = {
+  stopwatch: {
+    backgroundColor: 'clear',
+    padding: 5,
+    borderRadius: 5,
+    width: 220,
+  },
   container: {
     flex: 1,
     flexDirection: 'column',
