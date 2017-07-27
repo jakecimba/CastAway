dd39c14168bd396ab81f0fc338a49ea9003f3f78
Jake Cimbalista <jcimbalista@tonicdesign.com>
2017-07-27 13:43:24 -0400

    Added promises to componentWillMount for multiple channels

--- src/Components/AllPodcastsScreen.js
+++ src/Components/AllPodcastsScreen.js
@@ -18,36 +18,39 @@ class AllPodcastsScreen extends Component {
     title: 'All Podcasts',
   };
   
-  // componentWillMount() {
-  //   function storeXml(channels, storage) {
-  //     for (i = 0, len = channels.length; i < len; i++) {
-  //       fetchXml(channels[i].key)
-  //       .then((json) => {
-  //         storage.push(json)
-  //       }) 
-  //       .catch((error) => {
-  //         console.log(error)
-  //       })
-  //     }
-  //     return storage
-  //   }
-  //   let tempStorage = [];
-  //   storeXml(this.state.channels, tempStorage)
-  //   this.setState({feedData: tempStorage})
-  // }
+  componentWillMount() {
+    var storage = []
+    var channels = this.state.channels
+    var dailyPromise = fetchXml(channels[0].key)
+      .then((json) => {
+        return json
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    
+    var wtfPromise = fetchXml(channels[1].key)
+      .then((json) => {
+        return json
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+    Promise.all([dailyPromise, wtfPromise]).then(objects => {
+      console.log(objects)
+    })
+  }
   
   render() {
-    let feed = this.state.feedData;
-    // if (!feed) {
-    //   return (
-    //     <Text>Loading</Text>
-    //   )
-    // }
-    
+    let { feedData } = this.state.feedData
+    if (!feedData) {
+      return <Text> Loading </Text>
+    }
     const { navigate } = this.props.navigation;
     return (
       <FlatList
-        data={[{item: 'a'}]}
+        data={feedData}
         renderItem={(item) =>
           <TouchableOpacity onPress={() => navigate('PodcastDetail')} >
             <View style={styles.buttonContainer} >
@@ -56,7 +59,7 @@ class AllPodcastsScreen extends Component {
               source={{uri: 'https://dfkfj8j276wwv.cloudfront.net/images/01/1b/f3/d6/011bf3d6-a448-4533-967b-e2f19e376480/7fdd4469c1b5cb3b66aa7dcc9fa21f138efe9a0310a8a269f3dcd07c83a552844fcc445ea2d53db1e55d6fb077aeaa8a1566851f8f2d8ac4349d9d23a87a69f5.jpeg'}}
               />
               <View style={styles.textContainer}>  
-                <Text style={styles.podcastTitle}>The Daily</Text>
+                <Text style={styles.podcastTitle}>{item.rss.channel[0].title[0]}</Text>
                 <Text>The New York Times</Text>
               </View>
             </View>
